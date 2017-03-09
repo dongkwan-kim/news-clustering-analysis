e9e813785f5e87e5f88c46ff3b487e716cb61b3f
todoaskit <todoaskit@gmail.com>
2017-03-09 15:35:11 +0000

    Consider centrality

--- partition.py
+++ partition.py
@@ -1,7 +1,15 @@
 import networkx as nx
 import community
 from collections import defaultdict
-import pprint
+from pprint import pprint
+
+class N():
+    def __init__(self, name, centrality):
+        self.name = name
+        self.centrality = centrality
+
+    def __repr__(self):
+        return self.name + "-" + str(self.centrality)
 
 def get_nodes(fi):
     r_list = []
@@ -27,14 +35,43 @@ def get_graph(fi):
         g.add_edge(x, y, weight=e[(x, y)])
     return g
 
-def get_best_partition(fi):
-    g = get_graph(fi)
+def get_best_partition(g):
     part = community.best_partition(g)
+    return part
 
+def readable_partition(part):
     d = defaultdict(list)
     for (v, i) in part.items():
         d[i].append(v)
     return d
+
+def get_best_partition_fi(fi):
+    return readable_partition(get_best_partition(get_graph(fi)))
+
+def get_centrality(g):
+    return nx.eigenvector_centrality(g, max_iter=1000)
+
+def get_ind(g, part):
+    return community.induced_graph(part, g)
+
+def get_adj_list(g):
+    r_dict = defaultdict(float)
+    for nx, n_dict in g.edge.items():
+        for ny in n_dict.keys():
+            r_dict[ts(nx, ny)] = n_dict[ny]["weight"]
+    return r_dict
+
+def export_adj_list(adj_list, file_name="adj_list.csv"):
+    o = open(file_name, "w")
+    for ((x, y), w) in adj_list.items():
+        o.write(",".join([str(x), str(y), str(w), "\n"]))
+    o.close()
+
+def export_centrality(cent, file_name="centrality.csv"):
+    o = open(file_name, "w")
+    for (name, cv) in cent.items():
+        o.write(",".join([name, str(cv), "\n"]))
+    o.close
 
 def get_partition_num(part, emt):
     for i, l in part.items():
@@ -42,7 +79,30 @@ def get_partition_num(part, emt):
             return i
     return -1
 
+def ts(a, b):
+    return tuple(sorted([a, b]))
+
+def get_perfect_node(partition, centrality):
+    n_dict = {}
+    partition = readable_partition(partition)
+    for group, g_list in partition.items():
+        n_dict[group] = [N(name, centrality[name]) for name in g_list]
+    return n_dict
+
+
 if __name__ == "__main__":
     fi = "cooc_o.csv"
-    d = get_best_partition(fi)
-    pprint.pprint(d)
+
+    g = get_graph(fi)
+    p = get_best_partition(g)
+
+    cent = get_centrality(g)
+    export_centrality(cent)
+
+    ind = get_ind(g, p)
+    ind_adj_list = get_adj_list(ind)
+    export_adj_list(ind_adj_list, "induced_adj_list.csv")
+
+    pprint(readable_partition(p))
+
+    print(get_perfect_node(p, cent))
