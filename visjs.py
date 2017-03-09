309d7d124e1856aabf5d90b216976f81b2027ce7
todoaskit <todoaskit@gmail.com>
2017-03-09 11:40:45 +0000

    add partition to visjs

--- visjs.py
+++ visjs.py
@@ -1,11 +1,21 @@
-def create_visjs_network_from_raw(network):
+import random
+import partition as mp
+
+def r_color():
+    r = lambda: random.randint(0,255)
+    return '#%02X%02X%02X' % (r(),r(),r())
+
+
+def create_visjs_network_from_raw(network, partition):
     """
     :param network: dict {"(pid_x, pid_y)": "weight"}
     :return: tuple (node_list, edge_list)
     """
     node_list = []
     edge_list = []
-    node_color = {"background": "white", "border": "#455a64"}
+    nc_dict = {}
+    for i in partition.keys():
+        nc_dict[i] = {"background": r_color(), "border": "#455a64"}
 
     for x, y in sorted(network, key=network.get, reverse=True):
         if network[(x, y)] != 0:
@@ -17,11 +27,11 @@ def create_visjs_network_from_raw(network):
             node_x = {}
             node_x["id"] = x
             node_x["label"] = px
-            node_x["color"] = node_color
+            node_x["color"] = nc_dict[mp.get_partition_num(partition, px)]
             node_y = {}
             node_y["id"] = y
             node_y["label"] = py
-            node_y["color"] = node_color
+            node_y["color"] = nc_dict[mp.get_partition_num(partition, py)]
             if node_x not in node_list:
                 node_list.append(node_x)
             if node_y not in node_list:
@@ -30,23 +40,25 @@ def create_visjs_network_from_raw(network):
             edge = {}
             edge["from"] = x
             edge["to"] = y
-            edge["color"] = {"color": "green", "highlight": "green"}
+            edge["color"] = {"color": "grey", "highlight": "green"}
             edge["value"] = abs(weight)
             edge_list.append(edge)
 
     return (node_list, edge_list)
 
 
-def create_network():
+def create_network(fi):
     r_dict = {}
-    for line in open("network2_edge.csv", "r"):
+    for line in open(fi, "r"):
         arr = line.split(",")
         r_dict[tuple(sorted((arr[0], arr[1])))] = float(arr[2].strip())
     return r_dict
 
 
 if __name__ == "__main__":
-    n = create_network()
-    visjs = create_visjs_network_from_raw(n)
+    fi = "cooc_o.csv"
+    n = create_network(fi)
+    p = mp.get_best_partition(fi)
+    visjs = create_visjs_network_from_raw(n, p)
     print(visjs[0])
     print(visjs[1])
